3b7a9fd43e40a92a9ac5f7687cdb9309d6ea57dc
damien.leveugle@orange.fr <Damien>
2023-06-17 22:03:49 +0200

    flash message animation

---
+++ public/javascripts/documentReady.js
@@ -0,0 +1,6 @@
+$(document).ready(function() { 
+    $( ".flash-message" ).addClass( "animate--drop-in-fade-out" );
+    setTimeout(function(){
+        $( ".flash-message" ).removeClass( "animate--drop-in-fade-out" );
+    }, 3500);
+});
