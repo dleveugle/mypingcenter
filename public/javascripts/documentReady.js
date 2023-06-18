b8cd866d43e9e18b010e2343eb2e0fae4147e077
damien.leveugle@orange.fr <Damien>
2023-06-18 19:07:18 +0200

    Flash message animation coming from bottom

--- public/javascripts/documentReady.js
+++ public/javascripts/documentReady.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
     $( ".flash-message" ).addClass( "animate--drop-in-fade-out" );
     setTimeout(function(){
         $( ".flash-message" ).removeClass( "animate--drop-in-fade-out" );
-    }, 3500);
+    }, 2000);
 });
